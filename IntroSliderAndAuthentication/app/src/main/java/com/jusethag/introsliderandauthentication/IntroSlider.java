42ce74bf68ed7739a67dd1f2ee59881a3d3e595f
JusethAg <mauagueda@gmail.com>
2016-09-21 10:40:49 -0500

    Configure project's gradle file
    Butterknife dependency and some variables were added to both gradle
    file (project and module).

---
+++ IntroSliderAndAuthentication/app/src/main/java/com/jusethag/introsliderandauthentication/IntroSlider.java
@@ -0,0 +1,13 @@
+package com.jusethag.introsliderandauthentication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IntroSlider extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro_slider);
+    }
+}
